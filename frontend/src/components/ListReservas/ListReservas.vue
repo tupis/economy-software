0f98b97a0a62b760a63a2bb84956a94718dcd584
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-26 16:15:58 -0300

    feat: insert table

--- frontend/src/components/ListReservas/ListReservas.vue
+++ frontend/src/components/ListReservas/ListReservas.vue
@@ -1,17 +1,35 @@
 <template>
-  <div>
-    <div v-for="reserva in reservas" v-bind:key="reserva.id">
-      <router-link :to="reserva.linkPath">
-        <p>Status: {{ reserva.status }}</p>
-        <p>Numero de reserva: {{ reserva.numeroreserva }}</p>
-        <p>Numero de apartamento: {{ reserva.apartamento }}</p>
-        <p>Data de checkin: {{ reserva.datacheckin }}</p>
-        <p v-show="reserva.datacheckout">
-          Data de checkout: {{ reserva.datacheckout }}
-        </p>
-      </router-link>
-      <br />
-    </div>
+  <div class="container">
+    <v-table fixed-header>
+      <thead>
+        <tr>
+          <th class="text-left">Número de reserva</th>
+          <th class="text-left">Número de apartamento</th>
+          <th class="text-left">Status</th>
+          <th class="text-left">Data de checkin</th>
+          <th class="text-left">Data de checkout</th>
+          <th class="text-left">Opções</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="reserva in reservas" v-bind:key="reserva.id">
+          <td>{{ reserva.numeroreserva }}</td>
+          <td>{{ reserva.apartamento }}</td>
+          <td>{{ reserva.status }}</td>
+          <td>{{ reserva.datacheckin }}</td>
+          <td v-if="reserva.datacheckout">{{ reserva.datacheckout }}</td>
+          <td v-else>Ainda não registrado</td>
+          <td>
+            <button>
+              <router-link :to="reserva.linkEdit">Editar</router-link>
+            </button>
+            <button>
+              <router-link :to="reserva.linkPath">Ver Mais</router-link>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
   </div>
 </template>
 
@@ -23,3 +41,10 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: auto;
+}
+</style>
