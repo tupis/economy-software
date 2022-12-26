1ccf84d24fd76678027f7ef83a395bfed1406c2a
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-26 16:55:42 -0300

    feat: create more than one hospede

---
+++ frontend/src/pages/CreateReserva.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="container">
+    <v-alert type="success" v-show="sucess">Cadastrado com sucesso</v-alert>
+
+    <v-alert type="warning" v-show="error">Erro ao cadastrar hotel</v-alert>
+
+    <v-form ref="form">
+      <v-text-field
+        v-model="numeroreserva"
+        label="Número da reserva"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="apartamento"
+        label="Número do apartamentoo"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="datacheckin"
+        label="Data de Checkin"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="datacheckout"
+        label="Data de checkout"
+      ></v-text-field>
+
+      <v-text-field v-model="status" label="Status" required></v-text-field>
+
+      <button @click="addMoreHospede($event)">Adicionar mais um hóspede</button>
+      <div v-for="(hospede, index) in hospedes" v-bind:key="index">
+        <h3>Hospede:</h3>
+        <div class="wrapper-name">
+          <v-text-field
+            v-model="hospede.nome"
+            label="Nome"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="hospede.sobrenome"
+            label="Sobrenome"
+            required
+          ></v-text-field>
+        </div>
+      </div>
+
+      <div class="wrapper-buttons">
+        <v-btn color="success" @click="submit"> Cadastrar </v-btn>
+
+        <GoBackButton />
+      </div>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import HotelServices from "../services/hotelServices";
+import router from "../router";
+import GoBackButton from "../components/GoBackButton/GoBackButton.vue";
+const { create } = HotelServices;
+
+export default {
+  components: { GoBackButton },
+  data: () => ({
+    numeroreserva: "",
+    apartamento: "",
+    datacheckin: "",
+    datacheckout: "",
+    status: "",
+    hospedes: [
+      {
+        nome: "",
+        sobrenome: "",
+      },
+    ],
+    sucess: false,
+    error: false,
+  }),
+  methods: {
+    async submit() {
+      const params = {
+        idhotel: Number(this.getIdHotel()),
+        numeroreserva: this.numeroreserva,
+        apartamento: this.apartamento,
+        datacheckin: this.datacheckin,
+        datacheckout: this.datacheckout,
+        status: this.status,
+        hospedes: this.hospedes,
+      };
+      console.log(params);
+      //   const data = await create(params);
+      this.setMessage("sucess");
+    },
+    getIdHotel() {
+      return this.$route.params.id;
+    },
+    goBack() {
+      router.back();
+    },
+    setMessage(value) {
+      if (value === "sucess") {
+        this.sucess = true;
+        setTimeout(() => {
+          this.sucess = false;
+        }, 2000);
+      } else if (value === "error") {
+        this.error = true;
+        setTimeout(() => {
+          this.error = false;
+        }, 2000);
+      }
+    },
+    addMoreHospede(e) {
+      e.preventDefault();
+
+      this.hospedes.push({
+        nome: "",
+        sobrenome: "",
+      });
+    },
+  },
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 400px;
+  margin: auto;
+}
+
+.wrapper-buttons {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.wrapper-name {
+  display: flex;
+}
+</style>
