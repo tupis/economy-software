50707b3317fa3f401112b40fcc6126ce30f6aacf
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-26 01:36:09 -0300

    feat: use cors to local use

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/index";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = Number(process.env.PORT) || 5000;
 app.use(router);
 //TODO: validar cnpj
