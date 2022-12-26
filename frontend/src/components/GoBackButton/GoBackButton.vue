184cc91d759c6788b7f8bb3f4a76e7e280e52451
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-26 15:37:47 -0300

    feat: create go back buttoon

---
+++ frontend/src/components/GoBackButton/GoBackButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn color="info" @click="goBack"> Voltar </v-btn>
+</template>
+
+<script>
+import router from "../../router";
+
+export default {
+  name: "GoBackButton",
+  methods: {
+    goBack() {
+      router.back();
+    },
+  },
+};
+</script>
