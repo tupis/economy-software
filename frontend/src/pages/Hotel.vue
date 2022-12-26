de99fad16c6f283c49d81713cd42fd89635be1db
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-26 12:55:57 -0300

    feat: use component ListReserva

--- frontend/src/pages/Hotel.vue
+++ frontend/src/pages/Hotel.vue
@@ -9,27 +9,20 @@
 
     <h1>Reservas:</h1>
 
-    <div v-for="reserva in reservas" v-bind:key="reserva.id">
-      <router-link :to="reserva.linkPath">
-        <p>Status: {{ reserva.status }}</p>
-        <p>Numero de reserva: {{ reserva.numeroreserva }}</p>
-        <p>Numero de apartamento: {{ reserva.apartamento }}</p>
-        <p>Data de checkin: {{ reserva.datacheckin }}</p>
-        <p v-show="reserva.datacheckout">
-          Data de checkout: {{ reserva.datacheckout }}
-        </p>
-      </router-link>
-      <br />
-    </div>
+    <ListReservas :reservas="reservas" />
   </div>
 </template>
 
 <script>
+import ListReservas from "../components/ListReservas/ListReservas.vue";
 import HotelServices from "../services/hotelServices";
 const { search } = HotelServices;
 
 export default {
   name: "Hotel",
+  components: {
+    ListReservas,
+  },
   data() {
     return {
       hotelInfo: {},
