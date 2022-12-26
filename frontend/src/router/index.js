5184bef6b8cf6b03514313fa4f65a85091415596
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-26 15:39:56 -0300

    style: comment unused components

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Hotel from "../pages/Hotel.vue";
+import CreateHotel from "../pages/CreateHotel.vue";
+import EditHotel from "../pages/EditHotel.vue";
+// import CreateReserva from "../pages/CreateReserva.vue";
+// import EditReserva from "../pages/EditReserva.vue";
 import Reserva from "../pages/Reserva.vue";
-import ListHotel from "../pages/ListHotel.vue";
+import Home from "../pages/Home.vue";
 
 const routes = [
-  { path: "/", component: ListHotel },
-  { path: "/hotel/create", component: Hotel },
+  { path: "/", component: Home },
 
   { path: "/hotel/:id", component: Hotel },
-  { path: "/hotel/edit/:id", component: Hotel },
+  { path: "/hotel/create", component: CreateHotel },
+  { path: "/hotel/edit/:id", component: EditHotel },
 
   { path: "/reserva/:id", component: Reserva },
-  { path: "/reserva/create", component: Hotel },
-  { path: "/reserva/edit/:id", component: Hotel },
+  // { path: "/reserva/create", component: CreateReserva },
+  // { path: "/reserva/edit/:id", component: EditReserva },
 ];
 
 const router = createRouter({
