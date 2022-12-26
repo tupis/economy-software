b5783fc9936f40cb4888c60667812bfd08d1a48a
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-12-26 12:54:13 -0300

    feat: create component to list all reservas

---
+++ frontend/src/components/ListReservas/ListReservas.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="reserva in reservas" v-bind:key="reserva.id">
+      <router-link :to="reserva.linkPath">
+        <p>Status: {{ reserva.status }}</p>
+        <p>Numero de reserva: {{ reserva.numeroreserva }}</p>
+        <p>Numero de apartamento: {{ reserva.apartamento }}</p>
+        <p>Data de checkin: {{ reserva.datacheckin }}</p>
+        <p v-show="reserva.datacheckout">
+          Data de checkout: {{ reserva.datacheckout }}
+        </p>
+      </router-link>
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListReservas",
+  props: {
+    reservas: Array,
+  },
+};
+</script>
